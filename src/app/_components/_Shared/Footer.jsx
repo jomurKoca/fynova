ee0dcb34016f5bf33fb953c18ffee2b37828ef36
Ömür Koca <93597251+jomurKoca@users.noreply.github.com>
2025-02-20 12:39:21 +0100

    kurze Erklärung zu Footer

--- src/app/_components/_Shared/Footer.jsx
+++ src/app/_components/_Shared/Footer.jsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
-      <footer className="py-4 border-t border-blue-100 text-[#002855] ">
+      <footer className="py-4 border-t-2 border-blue-100 text-[#002855] ">
         <div className="flex flex-col-reverse md:flex-row sm:justify-center text-left mx-4 gap-4">
-          <p>© 2025 Fynova</p>
+          <p>© 2025 Fynova</p> 
           <a href="/terms" className="hover:underline">
             Impressum
           </a>
@@ -27,4 +27,11 @@ export default function Footer() {
       </footer>
     );
   }
-  
+  {/*
+    1- border-t ist eine TailwindCSS Klasse, die eine obere Border hinzufügt
+    2- flex-col ist eine TailwindCSS Klasse, die die Flexbox in eine Column umwandelt
+    3- reverse erlaubt es, die Reihenfolge der Elemente umzukehren
+    4- md:flex-row ermöglicht es uns, die Flexbox in eine Row umzuwandeln, wenn die Bildschirmgröße mindestens md ist
+    5- sm:justify-center zentriert die Elemente in der Flexbox, wenn die Bildschirmgröße mindestens sm ist
+    6- Mehrere Info für Bildschirmgrößen findest du in CSSTailwind.md 
+  */}
