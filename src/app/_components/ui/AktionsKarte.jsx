5851f961b556da34bfc698002edb56b7a667b651
Ömür Koca <93597251+jomurKoca@users.noreply.github.com>
2025-03-03 11:08:32 +0100

    Kleine Änderung für Rendering mit Amplify

--- src/app/_components/ui/AktionsKarte.jsx
+++ src/app/_components/ui/AktionsKarte.jsx
@@ -5,7 +5,7 @@ export default function AktionsKarte() {
     <div className="flex flex-col sm:flex-row py-10 sm:py-2 px-5">
       <Card className="w-96 cursor-pointer rounded-md">
         <CardContent className="p-0">
-          <img src="/fotos/aktionskarte.png" alt="Akti" className="w-full rounded-md h-full " />
+          <img src="/fotos/Aktionskarte1.png" alt="Akti" className="w-full rounded-md h-full " />
         </CardContent>
       </Card>
     </div>
